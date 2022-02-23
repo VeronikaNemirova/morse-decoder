364d45c8a90f6b34c6a5b11fa8bd8306d883f459
VeronikaNemirova <nvn92@tut.by>
2022-02-23 22:24:21 +0300

    feat: created decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10));
+    }
+    arr = arr.map(item => {
+        if (item === '**********') {
+            return ' ';
+        }
+        let newArr = [];
+        for (let i = 0; i < item.length; i += 2) {
+            newArr.push(item.slice(i, i + 2));
+        }
+        newArr = newArr.filter(element => element !== '00').map(element => element === '11' ? '-' : '.');
+        return MORSE_TABLE[newArr.join('')]
+    })
+    return arr.join('');
 }
 
 module.exports = {
